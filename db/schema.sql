612dd66d0057ec079e3255d1567f9a70740ac6a7
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-16 19:02:40 -0400

    minor tweaks on database

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary INT NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -22,9 +22,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role_id)
+    roles_id INT NOT NULL,
+    manager_id VARCHAR(30),
+    FOREIGN KEY (roles_id)
     REFERENCES roles(id)
     ON DELETE CASCADE
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,24 +4,24 @@ INSERT INTO department (title) VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal'),
-('Sales'),
+('Sales');
 
-INSERT INTO roles (title, salary, department_id) VALUES
-('Sales Lead', 100000, 4),
-('Salesperson', 80000, 4),
-('Lead Engineer', 150000, 1);
-('Software Enginer', 120000, 1),
-('Account Manager', 160000, 2),
-('Accountant', 125000, 2),
-('Legal Team Lead', 250000, 3),
+INSERT INTO roles (title, salary, department_id) VALUES 
+('Sales Lead', 100000, 4), 
+('Salesperson', 80000, 4), 
+('Lead Engineer', 150000, 1), 
+('Software Engineer', 120000, 1), 
+('Account Manager', 160000, 2), 
+('Accountant', 125000, 2), 
+('Legal Team Lead', 250000, 3), 
 ('Lawyer', 190000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('Leonardo', 'DaVinci', 1)
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES 
+('Leonardo', 'DaVinci', 1, NULL),
 ('Mike', 'Chan', 2, 'Leonardo DaVinci'),
-('Ashley', 'Rodriguez', 3),
+('Ashley', 'Rodriguez', 3, NULL),
 ('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
-('Kunal', 'Singh', 5),
+('Kunal', 'Singh', 5, NULL),
 ('Malia', 'Brown', 6, 'Kunal Singh'),
-('Sarah', 'Lourd', 7),
+('Sarah', 'Lourd', 7, NULL),
 ('Tom', 'Allen', 8, 'Sarah Lourd');
