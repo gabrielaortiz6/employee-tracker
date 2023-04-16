dd52ebdf99bbf08f9963cd3f6bd116b08d8149b2
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-16 18:13:45 -0400

    edited foreign keys

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,21 +6,22 @@ INSERT INTO department (title) VALUES
 ('Legal'),
 ('Sales'),
 
-INSERT INTO roles (title, salary) VALUES
-('Sales Lead', 100000),
-('Salesperson', 80000),
-('Lead Engineer', 150000);
-('Software Enginer', 120000),
-('Account Manager', 160000),
-('Accountant', 125000),
-('Legal Team Lead', 250000),
-('Lawyer', 190000);
+INSERT INTO roles (title, salary, department_id) VALUES
+('Sales Lead', 100000, 4),
+('Salesperson', 80000, 4),
+('Lead Engineer', 150000, 1);
+('Software Enginer', 120000, 1),
+('Account Manager', 160000, 2),
+('Accountant', 125000, 2),
+('Legal Team Lead', 250000, 3),
+('Lawyer', 190000, 3);
 
-INSERT INTO employee (first_name, last_name) VALUES 
-('Mike', 'Chan'),
-('Ashley', 'Rodriguez'),
-('Kevin', 'Tupik'),
-('Kunal', 'Singh'),
-('Malia', 'Brown'),
-('Sarah', 'Lourd'),
-('Tom', 'Allen'),
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Leonardo', 'DaVinci', 1)
+('Mike', 'Chan', 2, 'Leonardo DaVinci'),
+('Ashley', 'Rodriguez', 3),
+('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
+('Kunal', 'Singh', 5),
+('Malia', 'Brown', 6, 'Kunal Singh'),
+('Sarah', 'Lourd', 7),
+('Tom', 'Allen', 8, 'Sarah Lourd');
