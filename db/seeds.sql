b4191f417a605359ed5dbeb6f9dbfc414ed1ec29
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-15 23:38:00 -0400

    started seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+USE employees_db;
+
+INSERT INTO department (title) VALUES 
+('Engineering'),
+('Finance'),
+('Legal'),
+('Sales'),
+
+INSERT INTO roles (title, salary) VALUES
+('Sales Lead', 100000),
+('Salesperson', 80000),
+('Lead Engineer', 150000);
+('Software Enginer', 120000),
+('Account Manager', 160000),
+('Accountant', 125000),
+('Legal Team Lead', 250000),
+('Lawyer', 190000);
+
+INSERT INTO employee (first_name, last_name) VALUES 
+('Mike', 'Chan'),
+('Ashley', 'Rodriguez'),
+('Kevin', 'Tupik'),
+('Kunal', 'Singh'),
+('Malia', 'Brown'),
+('Sarah', 'Lourd'),
+('Tom', 'Allen'),
