ce1fe1d5268a264f435f44b083d46428a5050c84
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-19 16:16:03 -0400

    Final commit

--- db/schema.sql
+++ db/schema.sql
@@ -26,14 +26,3 @@ CREATE TABLE employee (
     FOREIGN KEY (roles_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
-
--- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- first_name VARCHAR(30) NOT NULL,
--- last_name VARCHAR(30) NOT NULL,
--- roles_id INT NOT NULL,
--- manager_id INT,
--- FOREIGN KEY (roles_id)
--- REFERENCES roles(id)
--- FOREIGN KEY (manager_id),
--- REFERENCES employee(id)
--- ON DELETE CASCADE
